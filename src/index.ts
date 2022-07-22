675d9510caaeef6d6eb5afd5576f763e37562e57
Kostiantyn Smyrnov <kostysh@gmail.com>
2022-07-22 20:38:48 +0200

    feat: 🎸 Added utility for creation of `Permit` signature

---
+++ src/index.ts
@@ -0,0 +1,57 @@
+import { ethers, Contract, Signature, Wallet, BigNumber } from 'ethers';
+
+export const createPermitSignature = async (
+  signer: Wallet,
+  contract: Contract,
+  owner: string,
+  spender: string,
+  value: BigNumber,
+  deadline: number
+): Promise<Signature> => {
+  const nonce = await contract.nonces(owner);
+  const name = await contract.name();
+  const version = '1';
+  const chainId = await signer.getChainId();
+
+  return ethers.utils.splitSignature(
+    await signer._signTypedData(
+      {
+        name,
+        version,
+        chainId,
+        verifyingContract: contract.address
+      },
+      {
+        Permit: [
+          {
+            name: 'owner',
+            type: 'address'
+          },
+          {
+            name: 'spender',
+            type: 'address'
+          },
+          {
+            name: 'value',
+            type: 'uint256'
+          },
+          {
+            name: 'nonce',
+            type: 'uint256'
+          },
+          {
+            name: 'deadline',
+            type: 'uint256'
+          }
+        ]
+      },
+      {
+        owner,
+        spender,
+        value,
+        nonce,
+        deadline
+      }
+    )
+  );
+};
