8fa0ed2dee932e56f56a386b2e32618d481df8f1
Kostiantyn Smyrnov <kostysh@gmail.com>
2022-07-22 10:43:59 +0200

    feat: 🎸 Implemented deployment script
    Deployment: erc20 and wrappedErc20, Ledger, Assets, WinPay

--- deploy/001.ts
+++ deploy/001.ts
@@ -7,7 +7,7 @@ import { utils } from 'ethers';
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployments, getNamedAccounts } = hre;
-  const { deploy } = deployments;
+  const { deploy, execute } = deployments;
 
   const { deployer, alice, bob, carol } = await getNamedAccounts();
 
@@ -21,12 +21,12 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const mockErc20Deploy = await deploy('MockERC20', {
     from: deployer,
     log: true,
-    autoMine: true // speed up deployment on local network, no effect on live network.
+    autoMine: true
   });
   const mockWrappedErc20Deploy = await deploy('MockWrappedERC20', {
     from: deployer,
     log: true,
-    autoMine: true // speed up deployment on local network, no effect on live network.
+    autoMine: true
   });
 
   if (mockErc20Deploy.newlyDeployed) {
@@ -39,9 +39,11 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
 
     // mint tokens to each address
     const NUM_TOKENS = utils.parseEther('1000000');
-    await erc20.mint(alice, NUM_TOKENS);
-    await erc20.mint(bob, NUM_TOKENS);
-    await erc20.mint(carol, NUM_TOKENS);
+    await Promise.all([
+      alice,
+      bob,
+      carol
+    ].map(address => erc20.mint(address, NUM_TOKENS)));
   }
 
   if (mockWrappedErc20Deploy.newlyDeployed) {
@@ -52,7 +54,72 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
     const erc20Factory = await ethers.getContractFactory('MockWrappedERC20');
     const wrappedErc20 = erc20Factory.attach(mockErc20Deploy.address) as MockWrappedERC20;
   }
+
+  const ledgerDeploy = await deploy('Ledger', {
+    from: deployer,
+    log: true,
+    autoMine: true
+  });
+
+  if (ledgerDeploy.newlyDeployed) {
+    console.log(
+      `Contract Ledger deployed at ${ledgerDeploy.address} using ${ledgerDeploy.receipt?.gasUsed} gas`
+    );
+  }
+
+  const assetErc20Deploy = await deploy('Asset', {
+    from: deployer,
+    log: true,
+    autoMine: true,
+    args: [ledgerDeploy.address, mockErc20Deploy.address, 0]
+  });
+
+  if (assetErc20Deploy.newlyDeployed) {
+    console.log(
+      `Contract Asset (erc20) deployed at ${assetErc20Deploy.address} using ${assetErc20Deploy.receipt?.gasUsed} gas`
+    );
+
+    await execute(
+      'Ledger',
+      { from: deployer, log: true },
+      'rely',
+      assetErc20Deploy.address
+    );
+  }
+
+  const assetWrappedErc20Deploy = await deploy('Asset', {
+    from: deployer,
+    log: true,
+    autoMine: true,
+    args: [ledgerDeploy.address, mockWrappedErc20Deploy.address, 1]
+  });
+
+  if (assetWrappedErc20Deploy.newlyDeployed) {
+    console.log(
+      `Contract Asset (wrappedErc20) deployed at ${assetWrappedErc20Deploy.address} using ${assetWrappedErc20Deploy.receipt?.gasUsed} gas`
+    );
+
+    await execute(
+      'Ledger',
+      { from: deployer, log: true },
+      'rely',
+      assetWrappedErc20Deploy.address
+    );
+  }
+
+  const winPayDeploy = await deploy('WinPay', {
+    from: deployer,
+    log: true,
+    autoMine: true,
+    args: [ledgerDeploy.address]
+  });
+
+  if (winPayDeploy.newlyDeployed) {
+    console.log(
+      `Contract WinPay deployed at ${winPayDeploy.address} using ${winPayDeploy.receipt?.gasUsed} gas`
+    );
+  }
 };
 
 export default func;
-func.tags = ['MockERC20', 'MockWrappedERC20'];
+func.tags = ['MockERC20', 'MockWrappedERC20', 'Ledger', 'Asset', 'WinPay'];
