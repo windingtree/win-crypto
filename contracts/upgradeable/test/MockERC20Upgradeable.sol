27d14c663603765a41d5638a66d2befae67c7192
Lint Action <lint-action@samuelmeuli.com>
2022-07-26 16:46:30 +0000

    Fix code style issues with Prettier

--- contracts/upgradeable/test/MockERC20Upgradeable.sol
+++ contracts/upgradeable/test/MockERC20Upgradeable.sol
@@ -10,46 +10,46 @@ import '@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20FlashMin
 
 /// @custom:security-contact security@windingtree.com
 contract MockERC20Upgradeable is
-    ERC20Upgradeable,
-    ERC20BurnableUpgradeable,
-    PausableUpgradeable,
-    AccessControlUpgradeable,
-    ERC20PermitUpgradeable,
-    ERC20FlashMintUpgradeable
+  ERC20Upgradeable,
+  ERC20BurnableUpgradeable,
+  PausableUpgradeable,
+  AccessControlUpgradeable,
+  ERC20PermitUpgradeable,
+  ERC20FlashMintUpgradeable
 {
-    bytes32 public constant PAUSER_ROLE = keccak256('PAUSER_ROLE');
-    bytes32 public constant MINTER_ROLE = keccak256('MINTER_ROLE');
+  bytes32 public constant PAUSER_ROLE = keccak256('PAUSER_ROLE');
+  bytes32 public constant MINTER_ROLE = keccak256('MINTER_ROLE');
 
-    function initialize() public initializer {
-        __ERC20_init('MockERC20', 'MTK');
-        __ERC20Burnable_init();
-        __Pausable_init();
-        __AccessControl_init();
-        __ERC20Permit_init('MockERC20');
-        __ERC20FlashMint_init();
+  function initialize() public initializer {
+    __ERC20_init('MockERC20', 'MTK');
+    __ERC20Burnable_init();
+    __Pausable_init();
+    __AccessControl_init();
+    __ERC20Permit_init('MockERC20');
+    __ERC20FlashMint_init();
 
-        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
-        _grantRole(PAUSER_ROLE, msg.sender);
-        _grantRole(MINTER_ROLE, msg.sender);
-    }
+    _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+    _grantRole(PAUSER_ROLE, msg.sender);
+    _grantRole(MINTER_ROLE, msg.sender);
+  }
 
-    function pause() public onlyRole(PAUSER_ROLE) {
-        _pause();
-    }
+  function pause() public onlyRole(PAUSER_ROLE) {
+    _pause();
+  }
 
-    function unpause() public onlyRole(PAUSER_ROLE) {
-        _unpause();
-    }
+  function unpause() public onlyRole(PAUSER_ROLE) {
+    _unpause();
+  }
 
-    function mint(address to, uint256 amount) public onlyRole(MINTER_ROLE) {
-        _mint(to, amount);
-    }
+  function mint(address to, uint256 amount) public onlyRole(MINTER_ROLE) {
+    _mint(to, amount);
+  }
 
-    function _beforeTokenTransfer(
-        address from,
-        address to,
-        uint256 amount
-    ) internal override whenNotPaused {
-        super._beforeTokenTransfer(from, to, amount);
-    }
+  function _beforeTokenTransfer(
+    address from,
+    address to,
+    uint256 amount
+  ) internal override whenNotPaused {
+    super._beforeTokenTransfer(from, to, amount);
+  }
 }
